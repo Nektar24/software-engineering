c09e0a13e21f187254064fecf082cdb7860f1d69
Periklis Mpouris <mpourisper@gmail.com>
2024-05-27 18:01:44 +0300

    admin second changes

--- CODE/cooltoor/app/src/main/java/com/cooltoor/Admin.java
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/Admin.java
@@ -1,8 +1,9 @@
-public class Admin{
+public class Admin extends User{
     private String expertise;
     private int admin_level;
 
-    public Admin(String expertise,int admin_level){
+    public Admin(String fullname,String username,String password,String email,long int phone_number,String address,String country,String birth_date,ArrayList<String> interests,String expertise,int admin_level){
+       super(String fullname,String username,String password,String email,long int phone_number,String address,String country,String birth_date,ArrayList<String> interests);
         this.expertise=expertise;
         this.admin_level=admin_level;
     }
