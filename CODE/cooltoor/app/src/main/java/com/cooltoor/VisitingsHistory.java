7a1e776f56ee67b15173856b692bd47760e621e3
Periklis Mpouris <mpourisper@gmail.com>
2024-05-27 17:20:07 +0300

    vistings history class on progress

---
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/VisitingsHistory.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class VisitingsHistory{
+    
+    private User visitings_history_user;
+    private ArrayList<HistoricPoint> visited_historic_points;
+
+
+    public VisitingsHistory(User visitings_history_user,ArrayList<historic_point> visited_historic_points){
+
+            this.visitings_history_user = visitings_history_user;
+            this.visited_historic_points = visited_historic_points;
+    }
+
+    public HistoricPoint getHistoricPoint(ArrayList<Float> l){
+        return this.visited_historic_points.get(location:l);
+    }
+
+    public void deleteHistoricPoint(ArrayList<Float> location){
+        this.visited_historic_points.delete(location:location);
+    }
+
+    public void modifyInDatabase(){
+        //TODO : modify in db
+    }
+}
