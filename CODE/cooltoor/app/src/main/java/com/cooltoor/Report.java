6fd84d6f12c8214f0a2045706fb5b2b5e8eb71ee
PERIKLIS <perilamp01@gmail.com>
2024-05-27 16:50:42 +0300

    report class constructor

--- CODE/cooltoor/app/src/main/java/com/cooltoor/Report.java
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/Report.java
@@ -1,7 +1,14 @@
 public class Report {
     
     private User report_user;
-    private HistoricPoint reported_historiv_point;
+    private HistoricPoint reported_historic_point;
     private String report_reason;
     private String report_text;
+
+    public Report(User report_user, HistoricPoint reported_historic_point, String report_reason, String report_text) {
+        this.report_user = report_user;
+        this.reported_historic_point = reported_historic_point;
+        this.report_reason = report_reason;
+        this.report_text = report_text;
+    }
 }
