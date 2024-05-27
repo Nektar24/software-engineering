041090e5b786e946007ce7247184b6efa794d889
PERIKLIS <perilamp01@gmail.com>
2024-05-27 18:12:16 +0300

    Admin ok

--- CODE/cooltoor/app/src/main/java/com/cooltoor/Admin.java
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/Admin.java
@@ -5,7 +5,7 @@ public class Admin extends User{
     private int admin_level;
 
     public Admin(String full_name, String username, String password, String email, long phone_number, String address, String country, String birth_date, ArrayList<String> interests, String expertise, int admin_level) {
-       super(String full_name, String username, String password, String email, long  phone_number, String address, String country, String birth_date, ArrayList<String> interests);
+        super(String full_name, String username, String password, String email, long  phone_number, String address, String country, String birth_date, ArrayList<String> interests);
         this.expertise = expertise;
         this.admin_level = admin_level;
     }
