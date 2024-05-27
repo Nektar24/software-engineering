a2d5e47258396c6c1b33040353e1bcc6e51b6c8f
Periklis Mpouris <mpourisper@gmail.com>
2024-05-27 18:42:54 +0300

    notification class first upload

---
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/Notification.java
@@ -0,0 +1,20 @@
+public class Notification{
+
+    private User notification_user;
+    private String title;
+    private String text;
+    private String date;
+
+    public Notification(User notification_user,String title,String text,String date){
+
+        this.notification_user=notification_user;
+        this.title=title;
+        this.text=text;
+        this.date=date;
+    }
+
+    public void storeInDatabase(){
+        DatabaseManager dbm = new DatabaseManager();
+        dbm.storeNotification(this);
+    }
+}
