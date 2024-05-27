b6a4bfca4ced69d0ed0e1d8051cec8ce9f8ea194
Nektarios <39172759+Nektar24@users.noreply.github.com>
2024-05-27 16:41:00 +0300

    location class almost done

---
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/Location.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Location {
+    private float lat ;
+    private float lon;
+    
+    public Location(float lan,float lon){
+
+        this.lan=lan;
+        this.lon=lon;
+    }
+
+    public String getCity(){
+        return "123";
+    }
+
+    public void updateLocation(ArrayList<Float> location){
+        this.lat = location.get(0);
+        this.lon = location.get(1);
+    }
+
+    public ArrayList<Float> getLocation(){
+        ArrayList<Float> l = new ArrayList<Float>;
+        l.add(this.lat);
+        l.add(this.lon);
+        return l;
+    }
+
+}
