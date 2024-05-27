eab7d347bb7601884fbd768bce9a10d6e4010bc2
Periklis Mpouris <mpourisper@gmail.com>
2024-05-27 18:27:53 +0300

    rating class on progress

---
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/Rating.java
@@ -0,0 +1,33 @@
+public class Rating{
+
+    private User rating_user;
+    private HistoricPoint rating_historic_point;
+    private int stars;
+    private String comment;
+
+    public Rating(User rating_user,HistoricPoint rating_historic_point,int stars,String comment){
+
+            this.rating_user=rating_user;
+            this.rating_historic_point=rating_historic_point;
+            this.stars=stars;
+            this.comment=comment;
+    }
+
+    public void storeInDatabase(){
+        DatabaseManager dbm = new DatabaseManager();
+        dbm.storeRating(this);
+    }
+
+    public void modifyInDatabase(){
+        DatabaseManager dbm = new DatabaseManager();
+        dbm.modifyRating(this);
+    }
+
+    public void setStars(int stars){
+        this.stars=stars;
+    }
+
+    public void setComment(String comment){
+        this.comment=comment;
+    }
+}
