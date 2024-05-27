f382619fcbc7d5655b103272a64ae7e6f0643d9b
Periklis Mpouris <mpourisper@gmail.com>
2024-05-27 19:03:47 +0300

    reservation class first upload

---
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/Reservation.java
@@ -0,0 +1,18 @@
+public class Reservation{
+
+    private Tour tour;
+    private User reservation_user;
+    private int seats;
+
+    public Reservation(Tour tour,User reservation_user,int seats){
+
+            this.tour=tour;
+            this.reservation_user=reservation_user;
+            this.seats=seats;
+    }
+
+    public void storeInDatabase(){
+        DatabaseManager dbm = new DatabaseManager();
+        dbm.storeNotification(this);
+    }
+}
