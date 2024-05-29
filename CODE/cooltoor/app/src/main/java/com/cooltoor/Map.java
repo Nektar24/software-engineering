5297ff2f3afe49c9a18b1bd0b84a81dca8500b93
KonstantinosPrt <konstantinos.pretsios24@gmail.com>
2024-05-29 17:29:59 +0300

    map class edited

--- CODE/cooltoor/app/src/main/java/com/cooltoor/Map.java
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/Map.java
@@ -1,5 +1,24 @@
 package com.cooltoor;
 
+import java.util.ArrayList;
+import java.io.*;
+
 public class Map {
-    Map
+    MapView map;
+
+    public Map(){
+        this.map = new MapView();
+    }
+
+    public Map(Location){
+        this.map = new MapView(Location);
+    }
+
+    public void addHistoricPoints(HistoricPointsList historic_points){
+        
+    }
+
+    public void addNavigationInstructions(NavigationInstructions instructions){
+        
+    }
 }
