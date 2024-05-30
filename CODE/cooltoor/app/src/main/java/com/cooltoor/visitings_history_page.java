9c4aa8ef0692eb69a222ed8e73af31848b16185d
Nektar24 <michailidisnektarios@gmail.com>
2024-05-30 14:44:49 +0300

    classes for pages

---
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/visitings_history_page.java
@@ -0,0 +1,17 @@
+package com.cooltoor;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class visitings_history_page extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.home_menu_page); // TODO change to visitings_history_page
+
+    }
+}
