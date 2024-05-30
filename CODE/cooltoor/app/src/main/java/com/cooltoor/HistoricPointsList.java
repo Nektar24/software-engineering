71640f269232ee2b28f9350659ffa8d8d3a033a9
KonstantinosPrt <konstantinos.pretsios24@gmail.com>
2024-05-30 16:46:26 +0300

    classes edited

--- CODE/cooltoor/app/src/main/java/com/cooltoor/HistoricPointsList.java
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/HistoricPointsList.java
@@ -1,5 +1,31 @@
 package com.cooltoor;
 
+import java.util.ArrayList;
+
 public class HistoricPointsList {
-    
+    private ArrayList<HistoricPoint> historic_points;
+
+    public HistoricPointsList(ArrayList<HistoricPoint> historic_points){
+        this.historic_points = historic_points;
+    }
+
+    public HistoricPoint getHistoricPoint(Location location){
+        for(HistoricPoint historic_point : this.historic_points){
+            if(historic_point.getPointCoordinates().equals(location)){
+                return historic_point;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<HistoricPoint> filterBasedOnSearch(String search){
+        ArrayList<HistoricPoint> filtered_points = new ArrayList<>();
+
+        for(HistoricPoint historic_point : this.historic_points){
+            if(historic_point.getTitle().contains(search) || historic_point.getText().contains(search)){
+                filtered_points.add(historic_point);
+            }
+        }
+        return filtered_points;
+    }
 }
--- CODE/cooltoor/app/src/main/java/com/cooltoor/HistoricWalksList.java
+++ CODE/cooltoor/app/src/main/java/com/cooltoor/HistoricWalksList.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import org.bson.Document;
 import java.io.*;
 
-/*
+
 public class HistoricWalksList {
     private ArrayList<HistoricWalk> historic_walks;
 
-    public HistoricWalksList(FindIterable<Document> walks) {
+    public HistoricWalksList(ArrayList<HistoricWalk> historic_walks) {
+        this.historic_walks = historic_walks;
+    }
 
-        for(Document doc : walks) {
-            
+    public HistoricWalk getHistoricWalk(String title){
+        for(HistoricWalk historic_walk : this.historic_walks){
+            if(historic_walk.getTitle().equals(title)){
+                return historic_walk;
+            }
         }
+        return null;
     }
 
     
 }
-*/
